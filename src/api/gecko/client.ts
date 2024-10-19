641cba6e474e2882ab0643458a1af07fec7e569a
Sebastian Pietras <me@spietras.dev>
2024-10-19 21:46:56 +0200

    Changed ports

--- src/api/gecko/client.ts
+++ src/api/gecko/client.ts
@@ -7,7 +7,7 @@ const scheme = process.env.MAGNOLIA__GECKO__HTTP__SCHEME || "http";
 const host = process.env.MAGNOLIA__GECKO__HTTP__HOST || "localhost";
 const port =
   process.env.MAGNOLIA__GECKO__HTTP__PORT === undefined
-    ? 31000
+    ? 10700
     : process.env.MAGNOLIA__GECKO__HTTP__PORT;
 const path = (process.env.MAGNOLIA__GECKO__HTTP__PATH || "")
   // Ensure path starts with a slash
